a2c0442e026ddfb1561482baa54170175f815133
Frederik Schubert <frederik.schubert@mailbox.org>
2021-08-24 16:08:10 +0200

    reduce dimensionality of BERT embeddings

--- bert_embeddings_experiment.py
+++ bert_embeddings_experiment.py
@@ -25,3 +25,26 @@ with torch.no_grad():
         natural_token_dict[token] = final_layer_embeddings[0]
 
 save_pkl(natural_token_dict, "natural_representations", prepath)
+
+#%%
+natural_tokens = torch.stack(list(natural_token_dict.values()))
+# %%
+import pymde
+import matplotlib.pyplot as plt
+from adjustText import adjust_text
+
+
+fig = plt.figure()
+embedding = pymde.preserve_distances(natural_tokens, embedding_dim=2, verbose=True).embed()
+ax = pymde.plot(embedding, marker_size=5)
+annotations = []
+for i, token_name in enumerate(token_names):
+    annotations.append(ax.annotate(token_name, (embedding[i, 0], embedding[i, 1])))
+adjust_text(annotations)
+
+# %%
+natural_token_dict_small = {}
+for token_name, e in zip(token_list, embedding):
+    natural_token_dict_small[token_name] = e
+save_pkl(natural_token_dict_small, "natural_representations_small", prepath)
+# %%
