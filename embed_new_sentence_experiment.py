f273ec3d2b7b1627e63383ad4e36960b9fa457de
awiszus <awiszus@tnt.uni-hannover.de>
2021-11-15 16:14:14 +0100

    added histogram visualization to embedding experiment

--- embed_new_sentence_experiment.py
+++ embed_new_sentence_experiment.py
@@ -2,13 +2,16 @@ from utils import load_pkl
 import torch
 import transformers
 import pymde
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 # Read new sentence
-new_sentence = "The sky above this ruin."
+new_sentence = "This stone brick wall is part of this ruin."
 
 # Load embeddings
-repr_dim = 8
+repr_dim = 32
 input_area_name = "ruins"
 block2repr = load_pkl(f"natural_representations_small_{repr_dim}",
                       f"/home/schubert/projects/World-GAN/input/minecraft/{input_area_name}/")
@@ -44,16 +47,33 @@ incremental_mde = pymde.preserve_distances(
     verbose=True)
 
 inc_embed = incremental_mde.embed()
-new_embedded_vec = inc_embed[-1]
+new_embedded_vec = inc_embed[-1] / torch.norm(inc_embed[-1], p=2)
+# new_embedded_vec = final_layer_embeddings[0].unsqueeze(0)
 
 # Calculate distances to old embeddings
 dists = torch.zeros(embeddings.shape[0])
 for i in range(embeddings.shape[0]):
     dists[i] = torch.norm(embeddings[i] - new_embedded_vec)
+    # dists[i] = torch.norm(nat_embeddings[i] - new_embedded_vec)
 
+keys = list(block2repr.keys())
+df = pd.DataFrame()
+dist_dict = {}
 for i, d in enumerate(dists):
-    print(f"{list(block2repr.keys())[i]} : {d}")
+    dist_dict[keys[i].replace('minecraft:', '')] = d.item()
+    print(f"{keys[i].replace('minecraft:', '')} : {d}")
+
+df = df.append(dist_dict, ignore_index=True)
 
 print(f"Closest token to sentence is: Nr. {dists.argmin()} - {list(block2repr.keys())[dists.argmin()]}")
 
 # Show results (in plot?)
+palette = "turbo"
+# Seaborn:
+plt.figure(figsize=(12, 8))
+
+sorted_names = sorted(dist_dict, key=dist_dict.get, reverse=False)
+p = sns.barplot(data=df, palette=palette, order=sorted_names)
+plt.title(f'Distances to "{new_sentence}"')
+plt.xticks(rotation=30, ha="right")
+plt.show()
