250d15fe92664a2d8275f9e74e31e44e8bfd129d
Frederik Schubert <frederik.schubert@mailbox.org>
2021-11-26 09:07:53 +0100

    update table generator

--- gather_results.py
+++ gather_results.py
@@ -3,9 +3,12 @@ from tap import Tap
 import json
 from torch import load as torch_load
 import pandas as pd
+import numpy as np
+
 
 class ResultConfig(Tap):
-    top_folder: str  # name of the folder containing multiple evaluated runs
+    # name of the folder containing multiple evaluated runs
+    top_folder: str = "/home/schubert/projects/World-GAN/output/ablation"
 
     def __init__(self, *args, underscores_to_dashes: bool = False, explicit_bool: bool = False, **kwargs):
         super().__init__(*args, underscores_to_dashes, explicit_bool, **kwargs)
@@ -14,39 +17,74 @@ class ResultConfig(Tap):
 if __name__ == '__main__':
     cfg = ResultConfig().parse_args()
 
-    df = pd.DataFrame()
-    for d in os.listdir(cfg.top_folder):
-        if os.path.isdir(os.path.join(cfg.top_folder, d)):
-            curr_files = os.path.join(cfg.top_folder, d, "files")
-            try:
-                with open(os.path.join(curr_files, "wandb-metadata.json")) as jsonfile:
-                    metadata = json.load(jsonfile)
-            except Exception:
-                print(f"No metadata for {d}")
-                continue
-            name = metadata["args"][-8].replace("_", " ")
-            try:
-                with open(os.path.join(curr_files, "random_samples", "results.json")) as jsonfile:
-                    results = json.load(jsonfile)
-                    reshape_results = {"TPKL-Div 5 mean":results["tpkldiv"]["mean"]["5"],
-                                       "TPKL-Div 5 var":results["tpkldiv"]["var"]["5"],
-                                       "TPKL-Div 10 mean":results["tpkldiv"]["mean"]["10"],
-                                       "TPKL-Div 10 var":results["tpkldiv"]["var"]["10"],
-                                       "Levenshtein mean":results["levenshtein"]["mean"],
-                                       "Levenshtein var":results["levenshtein"]["var"]}
-            except Exception:
-                print(f"No results for {d}")
-                reshape_results = {}
-            try:
-                result_entropy = torch_load(os.path.join(curr_files, "random_samples", "mean_entropy.pt"))
-                reshape_results["entropy mean"] = sum(result_entropy)/len(result_entropy)
-            except Exception:
-                print(f"No entropy for {d}")
+    variant_names = {
+        "bert_naive_8": "$\\text{World-GAN}$",
+        "bert_8": "$\\text{World-GAN}^{p}$",
+        "bert_naive_32": "$\\text{World-GAN}$",
+        "bert_32": "$\\text{World-GAN}^{p}$",
+    }
 
-            df = pd.concat([df, pd.DataFrame(reshape_results, index=[name])])
+    entropy_orig = {
+        "desert": 0.38,
+        "plains": 0.70,
+        "ruins": 1.12,
+        "beach": 0.61,
+        "swamp": 1.05,
+        "mine shaft": 0.85,
+        "village": 0.62
+    }
+
+    df = pd.DataFrame()
+    for variant in os.listdir(cfg.top_folder):
+        print(variant)
+        if not os.path.isdir(os.path.join(cfg.top_folder, variant)):
+            continue
+        for d in os.listdir(os.path.join(cfg.top_folder, variant)):
+            if os.path.isdir(os.path.join(cfg.top_folder, variant, d)):
+                curr_files = os.path.join(cfg.top_folder, variant, d, "files")
+                try:
+                    with open(os.path.join(curr_files, "wandb-metadata.json")) as jsonfile:
+                        metadata = json.load(jsonfile)
+                except Exception:
+                    print(f"No metadata for {d}")
+                    continue
+                name = metadata["args"][-8].replace("_",
+                                                    " ").replace("vanilla ", "").replace("simple ", "")
+                if name == "mineshaft":
+                    name = "mine shaft"
+                try:
+                    with open(os.path.join(curr_files, "random_samples", "results.json")) as jsonfile:
+                        results = json.load(jsonfile)
+                        reshape_results = {"TPKL-Div": np.mean(list(results["tpkldiv"]["mean"].values())),
+                                           "Levenshtein": results["levenshtein"]["mean"]}
+                except Exception:
+                    print(f"No results for {d}")
+                    reshape_results = {}
+                try:
+                    result_entropy = torch_load(os.path.join(
+                        curr_files, "random_samples", "mean_entropy.pt"))
+                    reshape_results["Entropy"] = sum(
+                        result_entropy)/len(result_entropy)
+                except Exception:
+                    print(f"No entropy for {d}")
+                reshape_results["Variant"] = variant_names[variant]
+                reshape_results["Structure"] = name
+                reshape_results["Entropy (orig.)"] = entropy_orig[name]
+                if "32" not in variant:
+                    continue
+
+                df = pd.concat(
+                    [df, pd.DataFrame(reshape_results, index=[name])])
 
     df = df.sort_index()
-    print(df.to_latex(float_format=lambda x: '%10.2f' % x))
+    df1 = df.pivot_table(["TPKL-Div", "Levenshtein"], ["Structure", "Variant"])
+    print(df.pivot_table(["TPKL-Div", "Levenshtein"], ["Variant"], aggfunc=np.mean).to_latex(float_format=lambda x: '%10.2f' % x, escape=False))
+    df2 = df.pivot_table(
+        ["Entropy"], ["Structure", "Entropy (orig.)"], "Variant")
+    print(df1.to_latex(float_format=lambda x: '%10.2f' % x, escape=False))
+    print("-----------------")
+    print(df.pivot_table(["Entropy"], [], "Variant", aggfunc=np.mean).to_latex(float_format=lambda x: '%10.2f' % x, escape=False))
+    print(df2.to_latex(
+        float_format=lambda x: '%10.2f' % x, escape=False))
     with open(os.path.join(cfg.top_folder, "tabular_results.tex"), "w") as txt_file:
         txt_file.write(df.to_latex(float_format=lambda x: '%10.2f' % x))
-
